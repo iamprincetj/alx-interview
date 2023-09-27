8beab7dea14b7db2b9e59769125d6ad6cc645b60
iamprincetj <tochukwunwanze5@gmail.com>
2023-09-27 19:54:53 +0100

    0-pascal_triangle.py done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    mylist = [1]
+    mylist1 = [[1]]
+    count = 0
+    while count != (n-1):
+        new_list = [1]
+        for i in range(len(mylist) - 1):
+            new_val = mylist[i] + mylist[i + 1]
+            new_list.append(new_val)
+        new_list.append(1)
+        mylist1.append(new_list)
+        mylist = new_list
+        count += 1
+    return mylist1
