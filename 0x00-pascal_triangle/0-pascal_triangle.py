cdc7014bcf02222c170ee8af40efceaf3eea3f82
iamprincetj <tochukwunwanze5@gmail.com>
2023-09-27 20:09:38 +0100

    0-pascal_triangle.py updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
 def print_triangle(n):
     """Returns a list of lists of integers representing the Pascal’s triangle of n"""
     if type(n) is not int:
