6ac25bcc4738234579b78a55886b1bec72c4d742
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-04 02:38:10 +0100

    My first commit && 0-lockboxes.py done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''Lock boxes '''
+
+
+def canUnlockAll(boxes):
+    ''' A method that determines if all the boxes can be opened '''
+
+    unlock = {0: "unlocked"}
+    keys = [key for key in boxes[0] if key not in unlock]
+    while keys:
+        new_keys = []
+        for key in keys:
+            if key not in unlock and key < len(boxes):
+                unlock[key] = "unlocked"
+                new_keys += [i for i in boxes[key] if i not in unlock]
+                keys = new_keys
+    return len(unlock) == len(boxes)
